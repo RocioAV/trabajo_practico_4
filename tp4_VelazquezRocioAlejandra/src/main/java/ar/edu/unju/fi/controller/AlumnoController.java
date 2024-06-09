3316e18b38080a521841e1a6a0433b206369df43
RocioAV <44351449@fi.unju.edu.ar>
2024-06-09 17:05:38 -0300

    Creacion de controllers correspondientes, para acceder a los formularios

--- tp4_VelazquezRocioAlejandra/src/main/java/ar/edu/unju/fi/controller/AlumnoController.java
+++ tp4_VelazquezRocioAlejandra/src/main/java/ar/edu/unju/fi/controller/AlumnoController.java
@@ -1,5 +1,18 @@
 package ar.edu.unju.fi.controller;
 
-public class AlumnoController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+
+@Controller
+@RequestMapping("/alumno")
+public class AlumnoController {
+	
+	@GetMapping("/form")
+	public String getNuevoAlumnoPage(Model model) {
+		model.addAttribute("titulo", "Nuevo Alumno");
+		return "formAlumno";
+	}
 }
--- tp4_VelazquezRocioAlejandra/src/main/java/ar/edu/unju/fi/controller/CarreraController.java
+++ tp4_VelazquezRocioAlejandra/src/main/java/ar/edu/unju/fi/controller/CarreraController.java
@@ -1,5 +1,19 @@
 package ar.edu.unju.fi.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/carrera")
 public class CarreraController {
 
+	
+	@GetMapping("/form")
+	public String getNuevaCarreraPage(Model model) {
+		model.addAttribute("titulo", "Nueva Carrera");
+		return "formCarrera";
+	}
+	
 }
